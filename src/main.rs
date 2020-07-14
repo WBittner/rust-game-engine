cce479415f648423175fb4d253ac6c060122051e
William Bittner <WilliamJohnBittner@gmail.com>
2020-07-14 18:48:04 -0400

    remove ex

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,16 @@ fn main() {
     let y = Entity::create_entity(12.0, 3.0);
     let y = Entity::create_entity(2.0, 33.0);
     println!("{:#?}", Entity::get_entities());
-    
+
     match Entity::get_entities().get_mut(&y) {
         Some(entity) => {
             entity.set_coords(123.123, 321.321);
         },
         None => ()
     }
+    println!("{:#?}", Entity::get_entities());
+
+    Entity::get_entities().remove(&y);
     println!("{:#?}", Entity::get_entities());
 
 }
